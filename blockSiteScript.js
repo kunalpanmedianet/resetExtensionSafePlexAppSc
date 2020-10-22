214ae6a5a0545d7784bf8faceb9805734844d4e3
nikhil.mate <nikhilmate2112@gmail.com>
2020-10-22 14:44:26 +0530

    blocksite UI done

--- blockSiteScript.js
+++ blockSiteScript.js
@@ -1,3 +1,8 @@
 var url = new URL(window.location.href);
-var blockedUrl = url.searchParams.get("blockedurl")
-document.body.innerHTML = blockedUrl + " has been blocked";
+var blockedUrl = url.searchParams.get('blockedurl');
+// document.body.innerHTML = blockedUrl + " has been blocked";
+var blockedSiteNameElement = document.getElementById('blockedSiteName');
+if (blockedSiteNameElement) {
+    if (blockedUrl) blockedSiteNameElement.textContent = blockedUrl;
+    else blockedSiteNameElement.textContent = 'This website';
+}

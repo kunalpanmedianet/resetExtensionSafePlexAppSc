1e3edacf7a3bfa6b2b0b050881e5f59cf6e55419
kunalpan <panchalkunal9498@gmail.com>
2020-10-27 23:54:42 +0530

    changes done

--- scripts/browsingData.js
+++ scripts/browsingData.js
@@ -1,16 +1,34 @@
 const loadEventForBrowsingData = (function () {
+	function isCheckedFn() {
+		const flags = [];
+		htmlUtil.each(htmlUtil('.browsingDataCheckBox'), function (index, item) {
+			const itemEl = $(item);
+			const isChecked = itemEl.prop('checked');
+			if (isChecked) {
+				flags.push(1);
+			}
+		});
+
+		if (flags.length > 0) {
+			htmlUtil('.clearData').removeClass('disabled');
+		} else {
+			htmlUtil('.clearData').addClass('disabled');
+		}
+	}
+
 	function loadCheckBoxChangeEvent() {
 		htmlUtil('.browsingDataCheckBox').on('change', function () {
 			const isChecked = htmlUtil(this).prop('checked');
 			if (!isChecked) {
 				htmlUtil('#allBrowsingData').prop('checked', false);
 			}
+			isCheckedFn();
 		});
 
 		htmlUtil('#allBrowsingData').on('change', function () {
 			const isChecked = htmlUtil(this).prop('checked');
-
 			htmlUtil('.browsingDataCheckBox').prop('checked', isChecked);
+			isCheckedFn();
 		});
 	}
 

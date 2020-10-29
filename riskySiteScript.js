5340208247e0f9daf1fc85219f135b130a313846
kunalpan <panchalkunal9498@gmail.com>
2020-10-29 15:15:18 +0530

    code reverted to fixes

--- riskySiteScript.js
+++ riskySiteScript.js
@@ -3,37 +3,38 @@ var riskyurl = url.searchParams.get('riskyUrl') || 'Nothing';
 // document.body.innerHTML = riskyurl + ' has been blocked';
 
 document.addEventListener('DOMContentLoaded', function () {
-    var proceedButton = document.getElementById('rsProceedAnyway');
+	var proceedButton = document.querySelector('.rsProceedAnyway');
 
-    if (!!proceedButton) {
-        proceedButton.addEventListener('click', function () {
-            console.log("button is clicked");
-            var blockedUrl="https://"+getBlockedUrl();
-            console.log("blocked url in risky site:",blockedUrl);
-            chrome.runtime.sendMessage({type: "unBlockRiskySite", blockedUrl: blockedUrl}, function () {
-            });
-        });
-    }
+	if (!!proceedButton) {
+		proceedButton.addEventListener('click', function () {
+			console.log('button is clicked');
+			var blockedUrl = 'https://' + getBlockedUrl();
+			console.log('blocked url in risky site:', blockedUrl);
+			chrome.runtime.sendMessage(
+				{ type: 'unBlockRiskySite', blockedUrl: blockedUrl },
+				function () {}
+			);
+		});
+	}
 });
 
-
 function getBlockedRiskyWebsiteName() {
-    var blockedUrl = getBlockedUrl();
-    // var urlDomain=getDomain(blockedUrl);
-    var riskySiteNameElement = document.getElementById('riskySiteName');
-    if (riskySiteNameElement) {
-        if (blockedUrl) riskySiteNameElement.textContent = blockedUrl;
-        else riskySiteNameElement.textContent = 'This website';
-    }
+	var blockedUrl = getBlockedUrl();
+	// var urlDomain=getDomain(blockedUrl);
+	var riskySiteNameElement = document.getElementById('riskySiteName');
+	if (riskySiteNameElement) {
+		if (blockedUrl) riskySiteNameElement.textContent = blockedUrl;
+		else riskySiteNameElement.textContent = 'This website';
+	}
 }
 
-function getBlockedUrl(){
-    var url = new URL(window.location.href);
-    return url.searchParams.get('riskyUrl');
+function getBlockedUrl() {
+	var url = new URL(window.location.href);
+	return url.searchParams.get('riskyUrl');
 }
 
 function getDomain(url) {
-    var domain = url.split("#")[0];
-    domain = domain.split("?")[0];
-    return domain;
+	var domain = url.split('#')[0];
+	domain = domain.split('?')[0];
+	return domain;
 }
